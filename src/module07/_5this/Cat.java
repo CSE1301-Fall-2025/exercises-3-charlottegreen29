d0745ab9eb954cd97de33f3c5ba65738971178c9
charlottegreen29 <c.j.green@wustl.edu>
2025-11-03 15:47:54 -0600

    added one quick thing from class

--- src/module07/_5this/Cat.java
+++ src/module07/_5this/Cat.java
@@ -9,10 +9,16 @@ public class Cat {
 
     //behaviors-->methods (meowing, running, eating, sleeping, dying, shedding, etc.)
 
+    //oh but first we need our constructor! that's a method we for sure need
+
     public Cat(String name, String voice){
         this.name = name; //"this"! (see notes on "this" in notes doc)
         this.voice = voice;
         age = 0; // we can have default values here if we want; there's lots of flexibility
     }
+
+    // main method time!
+    Cat lindsay = new Cat("lindsay", "mew");
+    Cat franklin = new Cat("franklin", "MEOWMEOWMEOWMEOW");
 
 }
