8ed4016b72cb69a4fb7c0b8d613cfe886a0765fc
charlottegreen29 <c.j.green@wustl.edu>
2025-11-17 15:07:01 -0600

    added lecture notes

--- src/module09/_01lists/UsingLists.java
+++ src/module09/_01lists/UsingLists.java
@@ -1,27 +1,34 @@
 package module09._01lists;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
-
-public interface List<T> {
-    public boolean add(T e);
-    public T get(int i);
-    public int indexOf(T e);
-	public boolean remove(T e);
-    public T remove(int i);
-    public int size();
-}
+import java.util.List;
 
 public class UsingLists {
 
-	public static void main(String[] args) {
-		List<String> eating = new LinkedList<String>(); //I'm really confused by what they're asking me to do; I'll fix it during the lecture
+	public static void main(String[] args) { //from lecture
 
-		eating.add("open mouth");
-		eating.add("insert food");
-		eating.add("chew");
-		eating.add("chew");
-		eating.add("swallow");
+		//creating an ArrayList and filling it with values (look how you don't need to set a size at the beginning!)
+		List <String> rainbow1 = new ArrayList<>();
+
+		rainbow1.add("Red");
+		rainbow1.add("Orange");
+		rainbow1.add("Yellow");
+		rainbow1.add("Green");
+		rainbow1.add("Blue");
+		rainbow1.add("Indigo");
+		rainbow1.add("Violet");
+
+		// some examples of ArrayList methods in action
+		System.out.println(rainbow1.size());
+		System.out.println(rainbow1.get(2));
+		System.out.println();
+
+		System.out.println(rainbow1); //it looks really nice! we don't have to iterate to see the contents like with a simple array!
 		
-		
+		rainbow1.set(5,"Purple");
+		rainbow1.set(6,"Pink");
+		System.out.println(rainbow1);
+
 	}
 }
